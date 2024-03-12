9536e4509a446975d5f5225c62a20432fa057eda
Tatiana <tmelnik03@gmail.com>
2024-03-12 11:09:10 +0300

    Комментарии

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace DBkp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            // Запуск приложения.
             Application.Run(new Form1()); 
         }
     }
